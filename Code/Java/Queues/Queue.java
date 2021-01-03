1d4a441c8d340ff27da967dff5acacb2463124ba
Adit Bala <adityarbala@gmail.com>
2021-01-02 21:46:16 -0500

    init Queue

---
+++ Code/Java/Queues/Queue.java
@@ -0,0 +1,20 @@
+public class Queue {
+
+    public LinkedList queue;
+    public int size;
+    static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
+    public int maxSize;
+    
+    public Queue() {
+      this(DEFAULT_MAX_SIZE);
+    }
+    
+    public Queue(int maxSize){
+      this.queue = new LinkedList();
+      this.size = 0;
+      this.maxSize = maxSize;
+    }
+      
+    public static void main(String[] args) {
+    }
+}
