4d87066bad92c1d4aaa48f74e5ea2657aeca2b9a
Adit Bala <adityarbala@gmail.com>
2021-01-07 21:38:07 -0500

    init Hashmap

---
+++ Code/Java/HashMaps/HashMap.java
@@ -0,0 +1,16 @@
+public class HashMap {
+
+    public String[] hashmap;
+    
+    public HashMap(int size) {
+          this.hashmap = new String[size];
+    }
+
+    public int hash(String key) {
+        int hashCode = 0;
+        for (int i = 0; i < key.length(); i++) {
+        hashCode = hashCode + Character.codePointAt(key, i);
+        }
+        return hashCode;
+    }
+}
