97442f682501ec62714edc5b3be430d64d0c0b33
Adit Bala <adityarbala@gmail.com>
2021-01-26 17:55:01 -0500

    finish Graphs

--- Code/Java/Graphs/Vertex.java
+++ Code/Java/Graphs/Vertex.java
@@ -4,8 +4,17 @@ public class Vertex {
     private String data;
     private ArrayList<Edge> edges;
 
-    public Vertex() {
+    public Vertex(String inputData) {
+        this.data = inputData;
+        this.edges = new ArrayList<Edge>();
+    }
 
+    public void addEdge(Vertex endVertex, Integer weight) {
+        this.edges.add(new Edge(this, endVertex, weight));
+    }
+
+    public void removeEdge(Vertex endVertex) {
+        this.edges.removeIf(edge -> edge.getEnd().equals(endVertex));
     }
 
     public String getData() {
