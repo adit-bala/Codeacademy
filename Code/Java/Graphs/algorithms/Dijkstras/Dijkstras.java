899609d31d991bd763b9e1c46f11f12adb2c7128
Adit Bala <adityarbala@gmail.com>
2021-01-31 12:09:09 -0500

    finish Dijkstra algorithm

--- Code/Java/Graphs/algorithms/Dijkstras/Dijkstras.java
+++ Code/Java/Graphs/algorithms/Dijkstras/Dijkstras.java
@@ -2,36 +2,62 @@ import java.util.*;
 
 public class Dijkstra {
 
-    public static Dictionary[] dijkstra(Graph g, Vertex startingVertex) {
+    public static Dictionary[] dijkstra (Graph g, Vertex startingVertex){
         Dictionary<String, Integer> distances = new Hashtable<>();
         Dictionary<String, Vertex> previous = new Hashtable<>();
-        // Write code for checkpoint 1 here:
+        PriorityQueue<QueueObject> queue = new PriorityQueue<QueueObject>();
 
-        for (Vertex v : g.getVertices()) {
-            distances.put(v.getData(), Integer.MAX_VALUE);
+        queue.add(new QueueObject(startingVertex, 0));
+
+        for (Vertex v: g.getVertices()) {
+            if(v != startingVertex){
+                distances.put(v.getData(), Integer.MAX_VALUE);
+            }
             previous.put(v.getData(), new Vertex("Null"));
         }
 
         distances.put(startingVertex.getData(), 0);
 
-        Vertex current = startingVertex;
 
-        for (Edge e : current.getEdges()) {
-            Integer alternate = e.getWeight() + distances.get(current.getData());
-            String neighborValue = e.getEnd().getData();
-            if (alternate < distances.get(neighborValue)) {
-                distances.put(neighborValue, alternate);
-                previous.put(neighborValue, current);
-                // Write code for checkpoint 3 here:
+        while(queue.size() != 0){
+            Vertex current = queue.poll().vertex;
+            for (Edge e: current.getEdges()) {
+                Integer alternate = distances.get(current.getData()) + e.getWeight();
+                String neighborValue = e.getEnd().getData();
+                if (alternate < distances.get(neighborValue)){
+                    distances.put(neighborValue, alternate);
+                    previous.put(neighborValue, current);
+                    queue.add(new QueueObject(e.getEnd(), distances.get(neighborValue)));
+                }
             }
         }
 
-        return new Dictionary[] { distances, previous };
+        return new Dictionary[]{distances, previous};
     }
 
-    public static void dijkstraResultPrinter(Dictionary[] d) {
+    public static void shortestPathBetween(Graph g, Vertex startingVertex, Vertex targetVertex){
+        Dictionary[] dijkstraDicts = dijkstra(g, startingVertex);
+        Dictionary distances = dijkstraDicts[0];
+        Dictionary previous = dijkstraDicts[1];
+        Integer distance = (Integer) distances.get(targetVertex.getData());
+        System.out.println("Shortest Distance between " + startingVertex.getData() + " and " + targetVertex.getData());
+        System.out.println(distance);
+
+        ArrayList<Vertex> path = new ArrayList<>();
+        Vertex v = targetVertex;
+        while(v.getData() != "Null"){
+            path.add(0,v);
+            v = (Vertex) previous.get(v.getData());
+        }
+        System.out.println("Shortest Path");
+        for (Vertex pathVertex: path){
+            System.out.println(pathVertex.getData());
+        }
+    }
+
+    public static void dijkstraResultPrinter(Dictionary[] d){
         System.out.println("Distances:\n");
-        for (Enumeration keys = d[0].keys(); keys.hasMoreElements();) {
+        for (Enumeration keys = d[0].keys(); keys.hasMoreElements();){
             String nextKey = keys.nextElement().toString();
             System.out.println(nextKey + ": " + d[0].get(nextKey));
         }
@@ -43,7 +69,7 @@ public class Dijkstra {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Graph testGraph = new Graph(true, true);
         Vertex a = testGraph.addVertex("A");
         Vertex b = testGraph.addVertex("B");
@@ -64,5 +90,6 @@ public class Dijkstra {
         testGraph.addEdge(e, g, -50);
 
         dijkstraResultPrinter(dijkstra(testGraph, a));
+        shortestPathBetween(testGraph, a, g);
     }
 }
